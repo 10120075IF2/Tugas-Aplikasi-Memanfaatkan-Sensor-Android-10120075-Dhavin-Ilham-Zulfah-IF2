df2f5f978309c2f407c5f3ffe69c28a2209da564
10120075IF2 <kevinrayman223@gmail.com>
2023-06-20 22:33:53 +0700

    10120075_IF2
    Tugas Aplikasi Memanfaatkan Sensor Android

---
+++ app/src/main/java/com/example/rekomendasirestoran/splash.java
@@ -0,0 +1,43 @@
+package com.example.rekomendasirestoran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+//NIM   : 10120075
+//NAMA  : DHAVIN ILHAM ZULFAH
+//KELAS : IF-2
+
+public class splash extends AppCompatActivity {
+
+    ImageView appname,splashimg;
+    LottieAnimationView lottieAnimationView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        lottieAnimationView.animate().translationY(1500).setDuration(1000).setStartDelay(5000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(splash.this,ViewPager.class));
+            }
+        },6000);
+    }
+}
---
+++ app/src/main/java/com/example/rekomendasirestoran/viewpageritem.java
@@ -0,0 +1,17 @@
+package com.example.rekomendasirestoran;
+
+//NIM   : 10120075
+//NAMA  : DHAVIN ILHAM ZULFAH
+//KELAS : IF-2
+
+public class viewpageritem {
+
+    int imageID;
+    String heading, description;
+
+    public viewpageritem(int imageID, String heading, String description) {
+        this.imageID = imageID;
+        this.heading = heading;
+        this.description = description;
+    }
+}
